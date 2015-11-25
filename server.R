17746b6dcd1404dc69525cd7d16cfb82c989e36a
duncan-palmer <duncan.palmer@sagebase.org>
2015-11-25 14:32:54 -0800

    Revised application to be single row upload and generalizable to any synapse table.

--- server.R
+++ server.R
@@ -1,18 +1,12 @@
 # Author : Duncan P.
 # shiny server code for nominateGene form. 
-
 # Function for generating a form for single gene submission!
 singleForm <- function(i){	
 		type <- cols[[i]]$columnType
 		name <- cols[[i]]$name
 		id <- paste0('id',name) 
 
-		if(name == 'comment'){
-			return(renderUI({
-				tags$textarea(placeholder='Description',id='idcomment',rows=6,cols=35)})
-				)}
-
-		else if(type == 'STRING' | type == 'ENTITYID' | type == 'LINK'){
+		if(type == 'STRING' | type == 'ENTITYID' | type == 'LINK'){
 			if(length(cols[[i]]$enumValues) > 0){
 				return(renderUI({
 					selectInput(id,name,choices=cols[[i]]$enumValues,selected=cols[[i]]$enumValues[1])}
@@ -21,8 +15,6 @@ singleForm <- function(i){
 				return(renderUI({
 					textInput(id,paste(name),value='')})
 			)}}
-		if(name == 'vote'){return(renderUI({}))}
-
 		else if(type == 'INTEGER' | type == 'DOUBLE'){
 			if(length(cols[[i]]$enumValues) > 0){
 				return(renderUI({selectInput(id,name,choices=cols[[i]]$enumValues,selected=cols[[i]]$enumValues[2])}
@@ -31,102 +23,38 @@ singleForm <- function(i){
 			return(renderUI({
 				numericInput(id,paste(name,'-',type),value=1)
 			}))}}
-
 		if(type == 'BOOLEAN'){
 			return(renderUI({
 				checkboxInput(id,name)
 			}))}
-
 		if(type == 'DATE'){
 			return(renderUI({
 				dateInput(id,name)
 			}))}
-
 		if(type == 'FILEHANDLEID'){
 			return(renderUI({
 				fileInput(id,name)})
 			)}}
-
-# Function to generate form for Group submission!
-GroupForm <- function(i){
-		if(i == 1){
-			return(renderUI({
-				fileInput('Group','Select File',accept='text/csv')}))}
-
-		else if(i==2){
-			return(renderUI({
-				radioButtons('separator','Separator',choices = list('TSV' = '\t', 'CSV' = ','))}))}
-
-		else if(i ==3){
-			return(renderUI({
-				checkboxInput('header','Header')}))}
-
-		else{return(renderUI({}))}
-			}
-
 shinyServer(
 	function(input,output){
-		# Code ran after submit button pressed
 		# Call renderUI for number of columns in table, with type of input box generated dependent on data type of column.
-		observeEvent(input$choice,
-			if(input$choice == 1){
-				output$uploadType <- renderUI({tags$h3('Single Upload')})
-				lapply(1:length(cols),function(i){
-				output[[paste0('entry',i)]] <- singleForm(i)})}	
+		lapply(1:length(cols),function(i){output[[paste0('entry',i)]] <- singleForm(i)})	
+		output$submit <- renderUI({actionButton('formSubmit','Submit')})
 
-			else{
-				output$uploadType <- renderUI({tags$h3('Group Upload')})
-				lapply(1:length(cols), function(i) {
-				output[[paste0('entry',i)]] <- GroupForm(i)})})	
-
-		# Form submit button.
-		output$submit <- renderUI({
-			actionButton('formSubmit','Submit')
-				})
-
-		# Upload form to synapse after user submits. 
 		observeEvent(input$formSubmit,{
-
+			withProgress(message='Uploading Gene Nomination(s)',{
 			# Get TableColumnList object and populate one row data frame with entries from form, set up colsId vector which 
 			# contains column I.D.'s of synapse table form is uploaded to.  
 			colsId <- as.numeric(length(cols))
 			for(i in 1:length(cols)){colsId[i] <- cols[[i]]$id}
-			schema <- TableSchema(name='genes',parent='syn5051764',columns=colsId)
-
-			if(input$choice == 1){
+				schema <- TableSchema(name='genes',parent='syn5051764',columns=colsId)
 				df <- data.frame(matrix(,nrow=1,ncol=0))
-			# iteratively add values input through HTML widgets to dataframe which will be used to create table row. 
-			# To-Do - process file inputs as necessary to provide compatibility with what synapse expects. Perhaps also error handling. 
-
 				for(i in 1:length(cols)){
-
-					if(cols[[i]]$name == 'vote'){value <- 1}
-					else if(cols[[i]]$columnType == 'INTEGER'){value <- as.numeric(input[[paste0('id',cols[[i]]$name)]])}
-					else{
-					value <- input[[paste0('id',cols[[i]]$name)]]}
-
-					df[[cols[[i]]$name]] <- value
-						}
-
+					if(cols[[i]]$columnType == 'INTEGER'){value <- as.numeric(input[[paste0('id',cols[[i]]$name)]])}
+					else{value <- input[[paste0('id',cols[[i]]$name)]]}
+					df[[cols[[i]]$name]] <- value}
 					print(df)
-					
-					# Define schema with parent project ID of table and columnID vector, create table w/ schema and dataframe, upload table.
-					# To-Do : change parentID to not be hardcoded. 
+					row_to_add <- synapseClient::Table(schema,df)
+					table <- synStore(row_to_add, retrieveData=TRUE)}
 
-					tableAdd <- synapseClient::Table(schema,df)
-					table <- synStore(tableAdd, retrieveData=TRUE)}
-			else{
-				inFile <- input$Group
-				file <- read.table(inFile$datapath,sep=input$separator,quote='',header=input$header)
-				if(!input$header){
-				#colNames <- c('geneName','nominee','comment')
-				colNames <- c('a','b','c')	
-				colnames(file) <- colNames}
-
-				print(file)	
-
-				tableAdd <- synapseClient::Table(schema,file)
-				table <- synStore(tableAdd, retrieveData=TRUE)
-
-				}
-})})		
+)})})		
--- ui.R
+++ ui.R
@@ -1,20 +1,10 @@
 # Author : Duncan P.
 # Shiny UI code for nominateGene form. 
-
 shinyUI(fluidPage(theme=shinytheme('flatly'),
-	sidebarLayout(
-
-	      sidebarPanel(h1('Gene Nomination'),br(),
-		# Radio buttons html widget for selecting type of upload, single or group.
-		radioButtons('choice',label='Single or Group Upload?',choices = list('Single' = 1,'Group' = 2), selected = 2)
-		), 
-
-	      mainPanel(
+		sidebarLayout(
+		sidebarPanel(helpText(paste('Synapse Table',tableId,sep=' '))),
+		mainPanel(
 		# Loop to output all entry fields of form, which are renderUI objects. 	
-			uiOutput('uploadType'),
-			lapply(1:(length(cols)), function(i){uiOutput(paste0('entry',i))}),
-			uiOutput('submit')
-			))
-))
-
-
+		lapply(1:(length(cols)), function(i){uiOutput(paste0('entry',i))}),
+		uiOutput('submit'))
+		)))
